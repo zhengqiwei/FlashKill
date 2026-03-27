ea69e4029f198bc2a58a362a11c423284b9ad8ff
郑淇玮 <14716170+zheng-qi-wei@user.noreply.gitee.com>
2026-03-27 09:48:01 +0800

    Initial commit

---
+++ src/main/java/cn/zqwei/flashkill/FlashKillApplication.java
@@ -0,0 +1,13 @@
+package cn.zqwei.flashkill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashKillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashKillApplication.class, args);
+    }
+
+}
